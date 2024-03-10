3580149086dd33d055a66b74e4baba15ccca9d32
Prem Kumar Modi <140635188+Premkumar-ji@users.noreply.github.com>
2024-03-10 15:39:28 +0530

    Update Find_prime_number.c

--- Find_prime_number.c
+++ Find_prime_number.c
@@ -1,21 +1,20 @@
 #include<stdio.h>
 void main()
 { 
-	int a,b,i;
+	int a,b,i=2;
 	printf("\nenter the number :   ");
-		scanf("%d",&a);
-		i=2;
-		while(i<=(a/2))
+	scanf("%d",&a);
+	while(i<=(a/2))
+	{
+		if(a%i==0)
 		{
-			if(a%i==0)
-			{
-				printf("\nnumber is not a prime number");
-				return 0;	  	
-			}
-			i=i+1;		
+			printf("\nnumber is not a prime number");
+			return 0;	  	
 		}
-			if(i==(a/2)+1)   //there is +1 because the while loop adds a more +1 value to "i" a the last call
-			{
-	  		    printf(" /nnumber is prime");
-			}
+		i=i+1;	
+	}
+	if(i==(a/2)+1)   //there is +1 because the while loop adds a more +1 value to "i" a the last call
+	{
+	    printf("\nnumber is prime");
+	}
 }
